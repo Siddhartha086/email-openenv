4f250096826ce60467575e5f958b2fbbe879e328
Siddhartha Nath <monty.nath7@gmail.com>
2026-03-30 05:30:17 +0000

    fix gradio launch

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import gradio as gr
+import os
+os.environ["GRADIO_SERVER_NAME"] = "0.0.0.0"
+os.environ["GRADIO_SERVER_PORT"] = "7860"
 
 # import your env safely
 env = None
@@ -71,4 +74,7 @@ with gr.Blocks() as demo:
 
 
 # 🚀 launch
-demo.launch(server_name="0.0.0.0", server_port=7860)
+demo.launch(server_name="0.0.0.0", server_port=7860, share=False)
+
+if __name__ == "__main__":
+    demo.launch(server_name="0.0.0.0", server_port=7860)
