413a157e7243f7b4d7cf75530617e9af1898549e
Siddhartha Nath <monty.nath7@gmail.com>
2026-03-30 14:28:23 +0000

    Finaly code is fine

---
+++ server/app.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from email_openenv.environment import EmailOpenEnv
+from email_openenv.models import ActionRequest
+
+app = FastAPI()
+env = EmailOpenEnv()
+
+@app.get("/")
+def root():
+    return {"status": "API is running"}
+
+@app.post("/reset")
+def reset():
+    return env.reset()
+
+@app.post("/step")
+def step(action: ActionRequest):
+    return env.step(action.dict())
+
+@app.get("/state")
+def state():
+    return env.state
